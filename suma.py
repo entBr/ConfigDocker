d0b6345ca4f818d63a87c73b53bddfa68477820b
entBr <brandonpro06@hotmail.com>
2024-10-03 21:17:58 -0600

    Actualizar programa

--- suma.py
+++ suma.py
@@ -1,12 +1,11 @@
-def main():
-    print("Calculadora simple de suma")
-    
-    num1 = float(input("Ingrese el primer número: "))
-    num2 = float(input("Ingrese el segundo número: "))
-    
-    resultado = num1 + num2
-    
-    print(f"Resultado: {resultado}")
 
-if __name__ == "__main__":
-    main
+print("Calculadora simple de suma")
+
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+
+resultado = num1 + num2
+
+print(f"Resultado: {resultado}")
+
+
